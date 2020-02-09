1d0c1e3d0b4c0bca00208851524faca7ebb25a97
peng <peng@qq.com>
2020-02-09 17:32:20 +0800

    新增了请求detailRecommend的地址

--- router/detailRouter.js
+++ router/detailRouter.js
@@ -1,7 +1,21 @@
 const { Router } = require("express");
 const mallmodel = require("../mallmodel/mallmodel.js");
+const mongoose = require("mongoose");
 
 const router = Router();
+
+router.get("/recommend", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  let id = mongoose.Types.ObjectId("5e3fb21c1f93ad1e7cc244d4");
+  mallmodel.findOne({ _id: id }, (err, data) => {
+    if (!err) {
+      res.send(data);
+      console.log("/detail/recommend,数据查找成功");
+    } else {
+      console.log("/detail/recommend,数据查询失败");
+    }
+  });
+});
 
 router.get("/", (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
